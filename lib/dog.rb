577fa50bbab3d760a21336a61cc4c51e2697cfe6
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 19:38:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,9 @@ class Dog
   # as an argument. key-value pairs need to contain id, name, and
   # breed.
   def initialize(argument_hash)
-
+    @id = id
+    @name = name
+    @breed = breed
   end
 
   def self.create_table
