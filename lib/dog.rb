e4dbaabbeb80016c36923273e2d32c7c6d321fc4
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 19:36:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,9 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  # accepts a hash or keyword argument value with key-value pairs as an argument. key-value pairs need to contain id, name, and breed.
+  # accepts a hash or keyword argument value with key-value pairs
+  # as an argument. key-value pairs need to contain id, name, and
+  # breed.
   def initialize
 
   end
