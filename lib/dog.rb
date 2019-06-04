b4a62eea56edc91bda2db8b969a8626765851581
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 20:04:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,9 +55,8 @@ class Dog
       LIMIT 1
     SQL
 
- DB[:conn].execute(sql, name).map do |row|
-   self.new_from_db(row)
- end.first
-
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
