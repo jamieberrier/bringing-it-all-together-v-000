8c90f0f664d2f1b475cc8f9e5c0d1ebbd54646af
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 19:41:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,8 @@
 class Dog
   attr_accessor :id, :name, :breed
-  
+
   # accepts a hash or keyword argument value with key-value pairs
-  # as an argument. key-value pairs need to contain id, name, and
-  # breed.
+  # as an argument. key-value pairs need to contain id, name, and breed.
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
